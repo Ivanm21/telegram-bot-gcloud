60f50d60f0cf3151404332aefab1d4b4c09dd9b1
unknown <ivanmalyshenko@gmail.com>
2020-05-25 20:27:41 +0300

    moved test_main.py

---
+++ test_main.py
@@ -0,0 +1,28 @@
+import main
+import unittest
+from unittest.mock import patch
+
+import flask
+from telegram import Bot
+
+class TestBot(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = flask.Flask(__name__)
+        pass
+
+    @classmethod
+    def tearDown(cls):
+        pass
+
+    def test_webhook(self):
+        with open('test/test_message.json') as f:
+            with patch.object(Bot, 'send_message', return_value='test message') as p_obj:
+                with self.app.test_request_context(method='POST', data=f):
+                    r = flask.request
+                    main.webhook(r)
+
+
+if __name__ == "__main__":
+    unittest.main()
