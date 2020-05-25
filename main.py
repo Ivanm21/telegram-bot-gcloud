f917b8ec1ec013f88928aacac6b689e77d21dd13
unknown <ivanmalyshenko@gmail.com>
2020-05-25 21:22:15 +0300

    Added Converstation

--- main.py
+++ main.py
@@ -38,15 +38,78 @@ def access_secret_version(project_id, secret_id, version_id='latest'):
     payload = response.payload.data.decode('UTF-8')
     return payload
 
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+
 def echo(update, context):
     text_to_send = f'{update.message.text}'
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_to_send)
+
+
+def start(update, context):
+    reply_keyboard = [['Boy', 'Girl', 'Other']]
+
+    update.message.reply_text(
+        'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Are you a boy or a girl?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+        )
+
+    return GENDER
+
+def gender(update, context):
+    user = update.message.from_user
+    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('I see! Please send me a photo of yourself, '
+                              'so I know what you look like, or send /skip if you don\'t want to.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return PHOTO
+
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 def setup(token):
     # Create bot, update queue and dispatcher instances
     bot = telegram.Bot(token=token)
     dispatcher = Dispatcher(bot, None, workers=0, use_context=True)
+
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)]
+
+            # PHOTO: [MessageHandler(Filters.photo, photo),
+            #         CommandHandler('skip', skip_photo)],
+
+            # LOCATION: [MessageHandler(Filters.location, location),
+            #            CommandHandler('skip', skip_location)],
+
+            # BIO: [MessageHandler(Filters.text, bio)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
     
     ##### Register handlers here #####
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
